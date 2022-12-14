1cc804a416d7366caa81c34057c63e5e61d45da6
sgupte45 <sgupte@sch.org>
2022-12-13 20:16:52 -0500

    added method to find shortest flight path
    May not even end up having to use it, it was fun to make tho

--- ARC_2021-2022/generatePlan.py
+++ ARC_2021-2022/generatePlan.py
@@ -1,6 +1,7 @@
 import os
-
-
+import numpy as np
+from itertools import permutations
+import math
 
 top_plate = "{\n\"fileType\": \"Plan\",\n\"geoFence\": {\n\"circles\": [\n],\n\"polygons\": [\n],\n\"version\": 2\n},\n\"groundStation\": \"QGroundControl\",\n\"mission\": {\n\"cruiseSpeed\": 15,\n\"firmwareType\": 12,\n\"globalPlanAltitudeMode\": 1,\n\"hoverSpeed\": 5,\n\"items\": ["
 
@@ -14,10 +15,46 @@ for line in lines:
     split = line.split() #Splits lines by spaces
     coordinates.append([split[0], split[2], split[4], split[5]])
 
+def search_home():
+    for x in coordinates:
+        if coordinates[coordinates.index(x)][3] == 'H':
+            return coordinates[coordinates.index(x)]
+
+home_set = search_home() # Remove apostrophes from this
 
 
+def optimize_path():
+    # First, create a set of all possible paths
+    # Then , Calculate net distance for each path
+    # Finally, Compare net distance between each thing
+    path = [[]]
+    k = 0
+    hx = home_set[0]
+    hy = home_set[1]
+    # Next part is a bit sketchy
+    xy = [[coordinates[1][0], coordinates[1][1]], [coordinates[2][0], coordinates[2][1]], [coordinates[3][0], coordinates[3][1]], [coordinates[4][0], coordinates[4][1]]]
+    points = permutations([xy[0], xy[1], xy[2], xy[3]], 4)
+    for p in list(points):
+        # is there a way to shorten the following mess
+        px0 = float(p[0][0].replace("'", ""))
+        py0 = float(p[0][1].replace("'", ""))
+        px1 = float(p[1][0].replace("'", ""))
+        py1 = float(p[1][1].replace("'", ""))
+        px2 = float(p[2][0].replace("'", ""))
+        py2 = float(p[2][1].replace("'", ""))
+        px3 = float(p[3][0].replace("'", ""))
+        py3 = float(p[1][0].replace("'", ""))
+        distance = math.dist([px0, py0], [px1, py1]) + math.dist([px1, py1], [px2, py2]) + math.dist([px2, py2], [px3, py3])
+        if (distance < k) or (k == 0):
+            k = distance
+            path = p
+    # Changing tuple to list so that it is mutable
+    lpath = [path[0], path[1], path[2], path[3]]
+    lpath.insert(0, [hx, hy])
+    lpath.append([hx, hy])
+    return lpath
 
-def generate_plan():
+def generate_plan(): #Eventually want to automate this
     f = open("test7.plan", "x")
     f.write(top_plate)
     f.write(generateObject(coordinates[0], "takeoff", 1))
@@ -49,7 +86,7 @@ def generateObject(coordinates, action, id): #CHANGE DOJUMPID FOR EACH THING
     else:
         return "ERROR: INVALID INPUT"
 
-generate_plan()
+
 
 
 
